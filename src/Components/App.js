62d9c07edbacaa4a02c7fb125d2b191fdd02aa91
Cátia <klisar1996@gmail.com>
2020-03-30 15:44:53 +0100

    Add components

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BtnContainer from "./BtnContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Color Game</h1>
+      </header>
+      <BtnContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/SquaresHard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../Style/Squares.css";
+import BtnHard from "./BtnHard";
+
+const SquaresHard = () => {
+    return(
+        <div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+        </div>
+    )
+}
+
+export default SquaresHard;
